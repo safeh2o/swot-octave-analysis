1398ef6efa3906518642f37829194f117762de20
saadface <53954183+saadface@users.noreply.github.com>
2019-08-14 22:17:19 -0400

    Add files via upload

--- Engineering Optimization Model/run.bat
+++ Engineering Optimization Model/run.bat
@@ -3,5 +3,5 @@
 set OCT_HOME=C:\Octave\Octave-5.1.0.0\mingw64\bin\
 set "PATH=%OCT_HOME%;%PATH%"
 
-set SCRIPTS_DIR=D:\Dropbox\Engineering Optimization Model\
+set SCRIPTS_DIR=D:\Dropbox\Saad\Engineering Optimization Model\
 start octave-cli-5.1.0.exe --eval "cd(getenv('SCRIPTS_DIR')); engmodel('Input/SWOT_TS and HH - latest version - False - 2019-07-30-01-27-31.xlsx','SWOT_TS and HH','Output'); quit;
