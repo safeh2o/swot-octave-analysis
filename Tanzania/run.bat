c475923805107d6dfcb9e0a31c1eb5edea774703
saadface <53954183+saadface@users.noreply.github.com>
2019-08-23 08:26:49 -0400

    Remove duplicated directory

--- Tanzania/run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set OCT_HOME=C:\Octave\Octave-5.1.0.0\mingw64\bin\
-set "PATH=%OCT_HOME%;%PATH%"
-
-set SCRIPTS_DIR=D:\Dropbox\Saad\Tanzania\
-start octave-cli-5.1.0.exe --eval "cd(getenv('SCRIPTS_DIR')); engmodel('Input/FRC Data Mtendeli-Tanzania_Sept16.xlsx','Team 2','Output'); quit;
