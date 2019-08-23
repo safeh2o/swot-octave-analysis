263b05c0f5871143c8d6add2bbd8f90698c1664c
saadface <53954183+saadface@users.noreply.github.com>
2019-08-23 08:28:04 -0400

    Remove duplicated directory

--- Jordan 2014/run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set OCT_HOME=C:\Octave\Octave-5.1.0.0\mingw64\bin\
-set "PATH=%OCT_HOME%;%PATH%"
-
-set SCRIPTS_DIR=D:\Dropbox\Saad\Jordan 2014\
-start octave-cli-5.1.0.exe --eval "cd(getenv('SCRIPTS_DIR')); engmodel('Input/Data_ChlorineRefugeeCamps_SIAli_May82018.xlsx','Jordan 2014','Output'); quit;
