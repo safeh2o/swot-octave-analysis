06e044cc1afc2c5e3f280e5d14cbe2feba9e6694
saadface <53954183+saadface@users.noreply.github.com>
2019-08-26 21:08:08 -0400

    Add files via upload

--- Engineering Optimization Model/engmodel.m
+++ Engineering Optimization Model/engmodel.m
@@ -1,9 +1,10 @@
 function engmodel(csvin,sheet,output)
-%2 point analysis
+%Engineering Optimization Model for SWOT
+%Version 1
+%Saad Ali
 clc
 format long
 pkg load statistics
-pkg load windows
 pkg load io
 
 %read data
@@ -11,33 +12,37 @@ pkg load io
 ##csv(1,:)=[];                  %delete header row
 [numdata strdata]=xlsread(csvin,sheet);
 for i=1:size(strdata,1)-1
-  hr=str2num(strdata{i+1,6}(12:13));
-  minute=str2num(strdata{i+1,6}(15:16));
-  second=str2num(strdata{i+1,6}(18:19));
-  se1tfull(i)=hr+minute/60+second/3600;
-  if ~isempty(strdata{i+1,10})
-    se1f(i)=str2num(strdata{i+1,10});
+  if ~isempty(strdata{i+1,6})
+    hr=str2num(strdata{i+1,6}(12:13));
+    minute=str2num(strdata{i+1,6}(15:16));
+    second=str2num(strdata{i+1,6}(18:19));
+    se1tfull(i)=hr+minute/60+second/3600; 
+  else
+    se1tfull(i)=-1;
   end
-
-  hr=str2num(strdata{i+1,30}(12:13));
-  minute=str2num(strdata{i+1,30}(15:16));
-  second=str2num(strdata{i+1,30}(18:19));
-  se2tfull(i)=hr+minute/60+second/3600;
-  if ~isempty(strdata{i+1,45})
-    se2f(i)=str2num(strdata{i+1,45});
+  
+  
+  if ~isempty(strdata{i+1,30})
+    hr=str2num(strdata{i+1,30}(12:13));
+    minute=str2num(strdata{i+1,30}(15:16));
+    second=str2num(strdata{i+1,30}(18:19));
+    se2tfull(i)=hr+minute/60+second/3600;
+  else
+    se2tfull(i)=-1;
   end
+  
 end
+se1f=numdata(:,10);
+se2f=numdata(:,45);
 se1tfull=se1tfull';
 se2tfull=se2tfull';
-se1f=se1f';
-se2f=se2f';
 
 se1t=(se1tfull-se1tfull);
 se2t=(se2tfull-se1tfull);
 
 se1fsave=se1f;
 se2fsave=se2f;
-bad=se2fsave<=0 | se1fsave <=0 | isnan(se2fsave);
+bad=se2fsave<=0 | se1fsave <=0 | isnan(se2fsave) | se1tfull<0 | se2tfull<0;
 se1fsave=se1fsave(bad==0);
 se2fsave=se2fsave(bad==0);
 
@@ -156,246 +161,7 @@ for i=1:5
 
   C10_1_test(i)=(0.2^(1-a_1(i,2))-(a_1(i,2)-1)*a_1(i,1)*10)^(1/(1-a_1(i,2)));
   C24_1_test(i)=(0.2^(1-a_1(i,2))-(a_1(i,2)-1)*a_1(i,1)*24)^(1/(1-a_1(i,2)));
-%%
-%FRC2 only model 2
-##[a_2 sse_2]=fminsearch(@fun5,k,'',t,f,f0,n5,n5only);
-##fpred=(f0.^(1-a_2(2))+(a_2(2)-1)*a_2(1)*t).^(1/(1-a_2(2)));
-##fpred(fpred<0.03)=0.03;
-##sse_2=sum((f-fpred).^2);
-##sst_2=sum((f-mean(f)).^2);
-##R2_2=1-sse_2/sst_2;
-##
-##sse_FRC2_2=sum((f(n5only+1:n5only+n5)-fpred(n5only+1:n5only+n5)).^2);
-##sst_FRC2_2=sum((f(n5only+1:n5only+n5)-mean(f(n5only+1:n5only+n5))).^2);
-##R2_FRC2_2=1-sse_FRC2_2/sst_FRC2_2;
-##
-##e1=fpred./f;
-##e2=f./fpred;
-##ee=[e1(e1>e2)-1 e2(e2>=e1)-1];
-##en=normpdf(ee,0.5,0.15);
-##SSR_2=sum(en);
-##
-##e1=fpred(n5only+1:n5only+n5)./f(n5only+1:n5only+n5);
-##e2=f(n5only+1:n5only+n5)./fpred(n5only+1:n5only+n5);
-##ee=[e1(e1>e2)-1 e2(e2>=e1)-1];
-##en=normpdf(ee,0.5,0.15);
-##SSR_FRC2_2=sum(en);
-##
-##res=f-fpred;
-##sumres_2=sum(res);
-##sumres_FRC2_2=sum(res(n5only+1:n5only+n5));
-##
-##%%
-##%relative error model 3
-##[a_3 sse_3]=fminsearch(@fun3,k,'',t,f,f0);
-##fpred=(f0.^(1-a_3(2))+(a_3(2)-1)*a_3(1)*t).^(1/(1-a_3(2)));
-##fpred(fpred<0.03)=0.03;
-##sse_3=sum((f-fpred).^2);
-##sst_3=sum((f-mean(f)).^2);
-##R2_3=1-sse_3/sst_3;
-##
-##sse_FRC2_3=sum((f(n5only+1:n5only+n5)-fpred(n5only+1:n5only+n5)).^2);
-##sst_FRC2_3=sum((f(n5only+1:n5only+n5)-mean(f(n5only+1:n5only+n5))).^2);
-##R2_FRC2_3=1-sse_FRC2_3/sst_FRC2_3;
-##
-##e1=fpred./f;
-##e2=f./fpred;
-##ee=[e1(e1>e2)-1 e2(e2>=e1)-1];
-##en=normpdf(ee,0.5,0.15);
-##SSR_3=sum(en);
-##
-##e1=fpred(n5only+1:n5only+n5)./f(n5only+1:n5only+n5);
-##e2=f(n5only+1:n5only+n5)./fpred(n5only+1:n5only+n5);
-##ee=[e1(e1>e2)-1 e2(e2>=e1)-1];
-##en=normpdf(ee,0.5,0.15);
-##SSR_FRC2_3=sum(en);
-##
-##%%
-##%at n=0 model 4
-##[a_4 sse_4]=fminsearch(@fun0,0.1,'',t,f,f0);
-##fpred=f0-a_4*t;
-##fpred(fpred<0.03)=0.03;
-##sst_4=sum((f-mean(f)).^2);
-##R2_4=1-sse_4/sst_4;
-##
-##sse_FRC2_4=sum((f(n5only+1:n5only+n5)-fpred(n5only+1:n5only+n5)).^2);
-##sst_FRC2_4=sum((f(n5only+1:n5only+n5)-mean(f(n5only+1:n5only+n5))).^2);
-##R2_FRC2_4=1-sse_FRC2_4/sst_FRC2_4;
-##
-##e1=fpred./f;
-##e2=f./fpred;
-##ee=[e1(e1>e2)-1 e2(e2>=e1)-1];
-##en=normpdf(ee,0.5,0.15);
-##SSR_4=sum(en);
-##
-##e1=fpred(n5only+1:n5only+n5)./f(n5only+1:n5only+n5);
-##e2=f(n5only+1:n5only+n5)./fpred(n5only+1:n5only+n5);
-##ee=[e1(e1>e2)-1 e2(e2>=e1)-1];
-##en=normpdf(ee,0.5,0.15);
-##SSR_FRC2_4=sum(en);
-##
-##res=f-fpred;
-##sumres_4=sum(res);
-##sumres_FRC2_4=sum(res(n5only+1:n5only+n5));
-##
-##%%
-##%at n=1 model 5
-##[a_5 sse_5]=fminsearch(@fun1,0.1,'',t,f,f0);
-##fpred=f0.*exp(-a_5*t);
-##fpred(fpred<0.03)=0.03;
-##sst_5=sum((f-mean(f)).^2);
-##R2_5=1-sse_5/sst_5;
-##
-##sse_FRC2_5=sum((f(n5only+1:n5only+n5)-fpred(n5only+1:n5only+n5)).^2);
-##sst_FRC2_5=sum((f(n5only+1:n5only+n5)-mean(f(n5only+1:n5only+n5))).^2);
-##R2_FRC2_5=1-sse_FRC2_5/sst_FRC2_5;
-##
-##e1=fpred./f;
-##e2=f./fpred;
-##ee=[e1(e1>e2)-1 e2(e2>=e1)-1];
-##en=normpdf(ee,0.5,0.15);
-##SSR_5=sum(en);
-##
-##e1=fpred(n5only+1:n5only+n5)./f(n5only+1:n5only+n5);
-##e2=f(n5only+1:n5only+n5)./fpred(n5only+1:n5only+n5);
-##ee=[e1(e1>e2)-1 e2(e2>=e1)-1];
-##en=normpdf(ee,0.5,0.15);
-##SSR_FRC2_5=sum(en);
-##
-##res=f-fpred;
-##sumres_5=sum(res);
-##sumres_FRC2_5=sum(res(n5only+1:n5only+n5));
-##
-##%%
-##%at n=2 model 6
-##[a_6 sse_6]=fminsearch(@fun2,0.1,'',t,f,f0);
-##fpred=(f0.^-1+a_6*t).^-1;
-##fpred(fpred<0.03)=0.03;
-##sst_6=sum((f-mean(f)).^2);
-##R2_6=1-sse_6/sst_6;
-##
-##sse_FRC2_6=sum((f(n5only+1:n5only+n5)-fpred(n5only+1:n5only+n5)).^2);
-##sst_FRC2_6=sum((f(n5only+1:n5only+n5)-mean(f(n5only+1:n5only+n5))).^2);
-##R2_FRC2_6=1-sse_FRC2_6/sst_FRC2_6;
-##
-##e1=fpred./f;
-##e2=f./fpred;
-##ee=[e1(e1>e2)-1 e2(e2>=e1)-1];
-##en=normpdf(ee,0.5,0.15);
-##SSR_6=sum(en);
-##
-##e1=fpred(n5only+1:n5only+n5)./f(n5only+1:n5only+n5);
-##e2=f(n5only+1:n5only+n5)./fpred(n5only+1:n5only+n5);
-##ee=[e1(e1>e2)-1 e2(e2>=e1)-1];
-##en=normpdf(ee,0.5,0.15);
-##SSR_FRC2_6=sum(en);
-##
-##res=f-fpred;
-##sumres_6=sum(res);
-##sumres_FRC2_6=sum(res(n5only+1:n5only+n5));
-##
-##%%
-##%weighted approach model 7
-##resmod(resmod<1e-6)=1;
-##w=1./resmod.^2;
-##
-##k=[0.001 2];
-##[a_7 sse_7]=fminsearch(@fun,k,'',t,f,f0,w);
-##fpred=(f0.^(1-a_7(2))+(a_7(2)-1)*a_7(1)*t).^(1/(1-a_7(2)));
-##fpred(fpred<0.03)=0.03;
-##sse_7=sum((f-fpred).^2);
-##sst_7=sum((f-mean(f)).^2);
-##R2_7=1-sse_7/sst_7;
-##
-##sse_FRC2_7=sum((f(n5only+1:n5only+n5)-fpred(n5only+1:n5only+n5)).^2);
-##sst_FRC2_7=sum((f(n5only+1:n5only+n5)-mean(f(n5only+1:n5only+n5))).^2);
-##R2_FRC2_7=1-sse_FRC2_7/sst_FRC2_7;
-##
-##e1=fpred./f;
-##e2=f./fpred;
-##ee=[e1(e1>e2)-1 e2(e2>=e1)-1];
-##en=normpdf(ee,0.5,0.15);
-##SSR_7=sum(en);
-##
-##e1=fpred(n5only+1:n5only+n5)./f(n5only+1:n5only+n5);
-##e2=f(n5only+1:n5only+n5)./fpred(n5only+1:n5only+n5);
-##ee=[e1(e1>e2)-1 e2(e2>=e1)-1];
-##en=normpdf(ee,0.5,0.15);
-##SSR_FRC2_7=sum(en);
-##
-##res=f-fpred;
-##sumres_7=sum(res);
-##sumres_FRC2_7=sum(res(n5only+1:n5only+n5));
-##
-##%%
-##%weighted approach, w=2 for FRC2, model 8
-##w=0.75*ones(1,length(t));
-##w(n5only+1:n5only+n5)=2;
-##
-##k=[0.001 2];
-##[a_8 sse_8]=fminsearch(@fun,k,'',t,f,f0,w);
-##fpred=(f0.^(1-a_8(2))+(a_8(2)-1)*a_8(1)*t).^(1/(1-a_8(2)));
-##fpred(fpred<0.03)=0.03;
-##sse_8=sum((f-fpred).^2);
-##sst_8=sum((f-mean(f)).^2);
-##R2_8=1-sse_8/sst_8;
-##
-##sse_FRC2_8=sum((f(n5only+1:n5only+n5)-fpred(n5only+1:n5only+n5)).^2);
-##sst_FRC2_8=sum((f(n5only+1:n5only+n5)-mean(f(n5only+1:n5only+n5))).^2);
-##R2_FRC2_8=1-sse_FRC2_8/sst_FRC2_8;
-##
-##e1=fpred./f;
-##e2=f./fpred;
-##ee=[e1(e1>e2)-1 e2(e2>=e1)-1];
-##en=normpdf(ee,0.5,0.15);
-##SSR_8=sum(en);
-##
-##e1=fpred(n5only+1:n5only+n5)./f(n5only+1:n5only+n5);
-##e2=f(n5only+1:n5only+n5)./fpred(n5only+1:n5only+n5);
-##ee=[e1(e1>e2)-1 e2(e2>=e1)-1];
-##en=normpdf(ee,0.5,0.15);
-##SSR_FRC2_8=sum(en);
-##
-##res=f-fpred;
-##sumres_8=sum(res);
-##sumres_FRC2_8=sum(res(n5only+1:n5only+n5));
-##
-##%%
-##%weighted approach, w=4 for FRC2, model 9
-##w=0.25*ones(1,length(t));
-##w(n5only+1:n5only+n5)=4;
-##
-##k=[0.001 2];
-##[a_9 sse_9]=fminsearch(@fun,k,'',t,f,f0,w);
-##fpred=(f0.^(1-a_9(2))+(a_9(2)-1)*a_9(1)*t).^(1/(1-a_9(2)));
-##fpred(fpred<0.03)=0.03;
-##sse_9=sum((f-fpred).^2);
-##sst_9=sum((f-mean(f)).^2);
-##R2_9=1-sse_9/sst_9;
-##
-##sse_FRC2_9=sum((f(n5only+1:n5only+n5)-fpred(n5only+1:n5only+n5)).^2);
-##sst_FRC2_9=sum((f(n5only+1:n5only+n5)-mean(f(n5only+1:n5only+n5))).^2);
-##R2_FRC2_9=1-sse_FRC2_9/sst_FRC2_9;
-##
-##e1=fpred./f;
-##e2=f./fpred;
-##ee=[e1(e1>e2)-1 e2(e2>=e1)-1];
-##en=normpdf(ee,0.5,0.15);
-##SSR_9=sum(en);
-##
-##e1=fpred(n5only+1:n5only+n5)./f(n5only+1:n5only+n5);
-##e2=f(n5only+1:n5only+n5)./fpred(n5only+1:n5only+n5);
-##ee=[e1(e1>e2)-1 e2(e2>=e1)-1];
-##en=normpdf(ee,0.5,0.15);
-##SSR_FRC2_9=sum(en);
-##
-##res=f-fpred;
-##sumres_9=sum(res);
-##sumres_FRC2_9=sum(res(n5only+1:n5only+n5));
-##
-##%%
 end
-##forxls=[a_1(1) a_1(2) sse_1 sse_FRC2_1 R2_1 R2_FRC2_1 sumres_1 sumres_FRC2_1 SSR_1 SSR_FRC2_1; a_2(1) a_2(2) sse_2 sse_FRC2_2 R2_2 R2_FRC2_2 sumres_2 sumres_FRC2_2 SSR_2 SSR_FRC2_2; a_3(1) a_3(2) sse_3 sse_FRC2_3 R2_3 R2_FRC2_3 sumres_3 sumres_FRC2_3 SSR_3 SSR_FRC2_3;  a_4 0 sse_4 sse_FRC2_4 R2_4 R2_FRC2_4 sumres_4 sumres_FRC2_4 SSR_4 SSR_FRC2_4; a_5 1 sse_5 sse_FRC2_5 R2_5 R2_FRC2_5 sumres_5 sumres_FRC2_5 SSR_5 SSR_FRC2_5; a_6 2 sse_6 sse_FRC2_6 R2_6 R2_FRC2_6 sumres_6 sumres_FRC2_6 SSR_6 SSR_FRC2_6; a_7(1) a_7(2) sse_7 sse_FRC2_7 R2_7 R2_FRC2_7 sumres_7 sumres_FRC2_7 SSR_7 SSR_FRC2_7; a_8(1) a_8(2) sse_8 sse_FRC2_8 R2_8 R2_FRC2_8 sumres_8 sumres_FRC2_8 SSR_8 SSR_FRC2_8; a_9(1) a_9(2) sse_9 sse_FRC2_9 R2_9 R2_FRC2_9 sumres_9 sumres_FRC2_9 SSR_9 SSR_FRC2_9];
 
   kmax=3*round(100*a_1(i,1))/100;
   fpred=[];
@@ -467,53 +233,21 @@ end
  saveas (gcf,sprintf('%s/backcheck.png',output))
  close all
  
-## ex1=sum(se1fsave>=0.2 & se1fsave<=0.5)
-## ex2=sum(se2fsave>=0.2 & se1fsave>=0.2 & se1fsave<=0.5)
-## ex2/ex1
-## pr1=sum(se1fsave>=C24_1(1) & se1fsave<=max(C))
-## pr2=sum(se2fsave>=0.2 & se1fsave>=C24_1(1) & se1fsave<=max(C))
-## pr2/pr1
- forxls=[k a_1 sse_1' sse_FRC2_1' R2_1' R2_FRC2_1' sumres_1' sumres_FRC2_1' SSR_1' SSR_FRC2_1' minC10' C10_1' maxC10' minC24' C24_1' maxC24'; k a_1 sse_1_test' sse_FRC2_1_test' R2_1_test' R2_FRC2_1_test' sumres_1_test' sumres_FRC2_1_test' SSR_1_test' SSR_FRC2_1_test' minC10' C10_1_test' maxC10' minC24' C24_1_test' maxC24'];
- csvwrite(sprintf('%s/Results.csv',output),forxls);
- xlswrite(sprintf('%s/Results.xlsx',output),forxls,'C2:T11');
+ forxls=cell(11,15);
+ forxls(1,:)={'','Initial guess for k','Initial guess for n','k','n','SSE','R2','Sum of residuals','Relative error','Minimum C(t=10h)','Optimum C(t=10h)','Maximum C(t=10h)','Minimum C(t=24h)','Optimum C(t=24h)','Maximum C(t=24h)'};
+ forxls(2)={'90% Training Set'};
+ forxls(7)={'10% Test Set'};
+ for i=1:5
+     forxls(1+i,2:15)={k(i,1) k(i,2) a_1(i,1) a_1(i,2) sse_1(i) R2_1(i) sumres_1(i) SSR_1(i) minC10(i) C10_1(i) maxC10(i) minC24(i) C24_1(i) maxC24(i)};
+   forxls(6+i,2:15)={k(i,1) k(i,2) a_1(i,1) a_1(i,2) sse_1_test(i) R2_1_test(i) sumres_1_test(i) SSR_1_test(i) minC10(i) C10_1_test(i) maxC10(i) minC24(i) C24_1_test(i) maxC24(i)};
+ end
+
+ %forxls=[k a_1 sse_1' R2_1' sumres_1' SSR_1' minC10' C10_1' maxC10' minC24' C24_1' maxC24'; k a_1 sse_1_test' R2_1_test' sumres_1_test' SSR_1_test' minC10' C10_1_test' maxC10' minC24' C24_1_test' maxC24'];
+ xlswrite(sprintf('%s/Results.xlsx',output),forxls,'A1:S11');
 end
 
 %looking at SSE for all points
 function SSE=fun(a,t,f,f0,w)
 fpred=(f0.^(1-a(2))+(a(2)-1)*a(1)*t).^(1/(1-a(2)));
 SSE=sum(w.*((f-fpred).^2));
-end
-
-%at n=0
-function SSE=fun0(a,t,f,f0)
-fpred=f0-a*t;
-SSE=sum((f-fpred).^2);
-end
-
-%at n=1
-function SSE=fun1(a,t,f,f0)
-fpred=f0.*exp(-a*t);
-SSE=sum((f-fpred).^2);
-end
-
-%at n=2
-function SSE=fun2(a,t,f,f0)
-fpred=(f0.^-1+a*t).^-1;
-SSE=sum((f-fpred).^2);
-end
-
-
-%only looking at SSE in FRC2
-function SSE_5=fun5(a,t,f,f0,n5,n)
-fpred=(f0.^(1-a(2))+(a(2)-1)*a(1)*t).^(1/(1-a(2)));
-SSE_5=sum((f(n+1:n+n5)-fpred(n+1:n+n5)).^2);
-end
-
-%relative error for all points
-function SSE_r=fun3(a,t,f,f0)
-fpred=(f0.^(1-a(2))+(a(2)-1)*a(1)*t).^(1/(1-a(2)));
-e1=fpred./f;
-e2=f./fpred;
-
-SSE_r=sum([e1(e1>e2)-1 e2(e2>=e1)-1]);
 end
--- Engineering Optimization Model/run.bat
+++ Engineering Optimization Model/run.bat
@@ -4,4 +4,4 @@ set OCT_HOME=C:\Octave\Octave-5.1.0.0\mingw64\bin\
 set "PATH=%OCT_HOME%;%PATH%"
 
 set SCRIPTS_DIR=D:\Dropbox\Saad\Engineering Optimization Model\
-start octave-cli-5.1.0.exe --eval "cd(getenv('SCRIPTS_DIR')); engmodel('Input/SWOT_TS and HH - latest version - False - 2019-07-30-01-27-31.xlsx','SWOT_TS and HH','Output'); quit;
+start octave-cli-5.1.0.exe --eval "cd(getenv('SCRIPTS_DIR')); engmodel('Input\SWOT_TS and HH - 2019-08-15.xlsx','SWOT_TS and HH','Output'); quit;
